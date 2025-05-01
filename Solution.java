50def0e65afc884b554c0abf3753f4577608e14d
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-01 09:30:18 +0900

    Day-4

--- Solution.java
+++ Solution.java
@@ -4,21 +4,20 @@ import org.w3c.dom.ls.LSOutput;
 
 class Solution {
 
-    // 나눗셈 로직
-    public int solution(int num1, int num2) {
-        int answer;
-    if(num1 > 0 && num1 <= 100 && num2 > 0 && num2 <= 100){
-        answer = (num1 / num2);
-    } else {
-        throw new IllegalArgumentException("숫자 범위를 확인해주세요.");
-    }
-        return answer;
-    }
+        public int solution(int age) {
+            int printYear;
+            if(age > 0 && age <= 120){
+                printYear = 2022 - (age-1);
+            } else {
+                throw new IllegalArgumentException("올바른 나이값이 아닙니다.");
+            }
+            return printYear;
+        }
 
-    // solution 출력메서드
-    void printSolution() {
-        System.out.println(solution(9,4));
-    }
+        public void showAge(int age) {
+            System.out.println("나이 : " + age);
+            System.out.println("출생연도 : " + solution(age));
+        }
 
 }
 
