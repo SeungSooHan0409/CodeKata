fd70e9ea9891a1ce56f422216441c2ffb340c03e
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-08 09:17:55 +0900

    숫자비교

--- CompareNumbers.java
+++ CompareNumbers.java
@@ -1,4 +1,21 @@
 package codekata;
 
 public class CompareNumbers {
+    public int solution(int num1, int num2) {
+        int result;
+        if(num1 >=0 && num1 <= 10000 && num2 >=0 && num2 <= 10000) {
+            if(num1 == num2) {
+                result = 1;
+            } else {
+                result = -1;
+            }
+        } else{
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
+        return result;
+    }
+
+    public void printSolution(int num1, int num2) {
+        System.out.println(solution(num1, num2));
+    }
 }
