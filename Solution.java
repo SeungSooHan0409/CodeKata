d551e403f6a7b14481eae606618bc05a1e376b50
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-09 09:25:30 +0900

    6

--- Solution.java
+++ Solution.java
@@ -4,19 +4,16 @@ import org.w3c.dom.ls.LSOutput;
 
 class Solution {
 
-        public int solution(int num1, int num2) {
-            int result;
-            if(num1 >=0 && num1 <= 10000 && num2 >=0 && num2 <= 10000) {
-                if(num1 == num2) {
-                    result = 1;
-                } else {
-                    result = -1;
-                }
-            } else{
-                throw new IllegalArgumentException("잘못된 입력값입니다.");
-            }
-            return result;
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        boolean rangeOfNumber = (-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000);
+        if(rangeOfNumber){
+            answer = num1 + num2;
+        } else {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
         }
+        return answer;
+    }
 
         public void printSolution(int num1, int num2) {
             System.out.println(solution(num1, num2));
