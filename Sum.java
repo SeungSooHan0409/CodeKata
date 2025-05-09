2ccd22b8feedbe1aada0525237a773fb4c13da29
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-09 09:26:14 +0900

    두수의합

--- Sum.java
+++ Sum.java
@@ -1,4 +1,18 @@
 package codekata;
 
 public class Sum {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        boolean rangeOfNumber = (-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000);
+        if(rangeOfNumber){
+            answer = num1 + num2;
+        } else {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
+        return answer;
+    }
+
+    public void printSolution(int num1, int num2) {
+        System.out.println(solution(num1, num2));
+    }
 }
