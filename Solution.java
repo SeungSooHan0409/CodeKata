c369c534e3b7c79072a2e7008e4f5f6cbc2e15d4
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-13 09:15:39 +0900

    8

--- Solution.java
+++ Solution.java
@@ -4,25 +4,17 @@ import org.w3c.dom.ls.LSOutput;
 
 class Solution {
 
-    public int solution(int angle) {
+    public int solution(int n) {
         int answer = 0;
-        if(0 < angle && angle < 90){
-            answer = 1;
-        } else if(angle == 90){
-            answer = 2;
-        } else if(90 < angle && angle < 180){
-            answer = 3;
-        } else if (angle == 180){
-            answer = 4;
-        } else{
-            throw new IllegalArgumentException("올바른 값을 입력해주세요.");
+        if(0<n && n<=1000){
+            for(int i=0; i<=n; i++){
+                if(i%2==0){
+                    answer += i;
+                }
+            }
         }
         return answer;
     }
-
-        public void printSolution(int angle) {
-            System.out.println(solution(angle));
-        }
 
 }
 
