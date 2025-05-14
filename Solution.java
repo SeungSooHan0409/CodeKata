0fd212fade5e01732d9511be921f52d8147c096d
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-14 09:39:08 +0900

    9

--- Solution.java
+++ Solution.java
@@ -4,18 +4,18 @@ import org.w3c.dom.ls.LSOutput;
 
 class Solution {
 
-    public int solution(int n) {
-        int answer = 0;
-        if(0<n && n<=1000){
-            for(int i=0; i<=n; i++){
-                if(i%2==0){
-                    answer += i;
-                }
+    public double solution(int[] numbers) {
+        double answer = 0;
+        double stackOfNum = 0;
+        for(int i = 0; i < numbers.length; i++){
+            if(0 <= numbers[i] && numbers[i] <= 1000 && 1 <= numbers.length && numbers.length <= 100) {
+                stackOfNum += numbers[i];
             }
         }
+        answer = stackOfNum/numbers.length;
         return answer;
-    }
 
+    }
 }
 
 
