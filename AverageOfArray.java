6db90e616a4de0379f70f052dd73621204739fc4
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-14 09:39:39 +0900

    배열의평균값

---
+++ AverageOfArray.java
@@ -0,0 +1,16 @@
+package codekata;
+
+public class AverageOfArray {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        double stackOfNum = 0;
+        for(int i = 0; i < numbers.length; i++){
+            if(0 <= numbers[i] && numbers[i] <= 1000 && 1 <= numbers.length && numbers.length <= 100) {
+                stackOfNum += numbers[i];
+            }
+        }
+        answer = stackOfNum/numbers.length;
+        return answer;
+
+    }
+}
