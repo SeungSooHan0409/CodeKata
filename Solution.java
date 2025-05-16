5b7870528e123fdec2ccc7c0ae41b479154f4eba
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-16 09:23:04 +0900

    11

--- Solution.java
+++ Solution.java
@@ -4,15 +4,15 @@ import org.w3c.dom.ls.LSOutput;
 
 class Solution {
 
-    public String solution(int num) {
-        String answer = "";
-        if(num%2 == 1) {
-            answer = "Odd";
-        } else if(num%2 == 0) {
-            answer = "Even";
+        public int solution(double num1, double num2) {
+            double resultOfCalculation = 0;
+            if(0<num1 && num1<=100 && 0<num2 && num2<=100){
+                resultOfCalculation = (num1/num2)*1000;
+            }
+            int answer = (int) resultOfCalculation;
+            return answer;
         }
-        return answer;
-    }
+
 }
 
 
