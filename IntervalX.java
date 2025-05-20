d654383d4b2c6fa799726df9b6828f0bff79bfdd
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-20 10:01:39 +0900

    x만큼간격이있는n개의숫자

---
+++ IntervalX.java
@@ -0,0 +1,15 @@
+package codekata;
+
+public class IntervalX {
+
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        if(-10000000 <= x && x <= 10000000 && 0 < n && n <= 1000) {
+            for (int i = 0 ; i < n ; i++) {
+                answer[i] = (long)(i+1)*x;
+            }
+        }
+        return answer;
+    }
+
+}
