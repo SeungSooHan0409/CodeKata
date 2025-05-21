7ef93498f7ffb7a96c3a2546403c0243ee9f753e
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-21 09:53:38 +0900

    17

--- Solution.java
+++ Solution.java
@@ -4,14 +4,22 @@ import org.w3c.dom.ls.LSOutput;
 
 class Solution {
 
-    public long[] solution(int x, int n) {
-        long[] answer = new long[n];
-        if(-10000000 <= x && x <= 10000000 && 0 < n && n <= 1000) {
-            for (int i = 0 ; i < n ; i++) {
-                answer[i] = (long)(i+1)*x;
+    public int[] solution(long n) {
+        long copyOfN = n;
+        if(0 < n && n <= 10000000000L) {
+            int i = 0;
+            for(i = 0; n != 0; i++) {
+                n /= 10;
             }
+            int[] answer = new int[i];
+            for(i = 0; copyOfN != 0; i++) {
+                answer[i] = (int)(copyOfN%10);
+                copyOfN /= 10;
+            }
+            return answer;
+        } else {
+            throw new IllegalArgumentException("올바른 값을 입력해주세요.");
         }
-        return answer;
     }
 
 }
