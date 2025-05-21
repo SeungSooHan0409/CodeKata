f01ba17d3e95b8b7485af169eebb11d97a119238
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-21 10:03:03 +0900

    문자열을정수로바꾸기

---
+++ ConvertStringIntoInt.java
@@ -0,0 +1,11 @@
+package codekata;
+
+public class ConvertStringIntoInt {
+
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.valueOf(s);
+        return answer;
+    }
+
+}
