43be3f136a9c53c904b401a35d54c74a0b3da4f4
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-22 21:54:14 +0900

    21

--- Solution.java
+++ Solution.java
@@ -7,28 +7,20 @@ import java.util.Arrays;
 
 class Solution {
 
-    public long solution(long n) {
-        long answer = 0;
-        int i = 0;
-        // n의 자릿수 세기
-        for(i = 1; (long)(n/Math.pow(10,i)) != 0;i++) {
-        }
-
-        // 자릿수 만큼의 길이를 가진 배열 만들기
-        long[] arr = new long[i];
-
-        //배열에 값넣기
-        for (int j = 0; j < i ; j++) {
-            arr[j] = n%10;
-            n/=10;
-        }
-
-        // 배열을 오름차순으로 정렬하기 - Arrays 를 import 하지않으니 안돌아갔음.
-        Arrays.sort(arr);
-
-        // 정렬한 배열로 숫자만들기
-        for (i = 0; i < arr.length; i++) {
-            answer += (arr[i])*Math.pow(10,i);
+    public boolean solution(int x) {
+        boolean answer = true;
+        int SumOfDigits = 0;
+        int x2 = x;
+        if(0 < x && x <= 10000) {
+            // 자릿수의 합을 구하는 메서드 구현
+            for(int i = 1; (x2)%10 != 0 || (x2)/10 != 0; i++) {
+                SumOfDigits += (x2)%10;
+                (x2)/=10;
+            }
+            // 나머지가 생기면 false 를 반환.
+            answer = (x%SumOfDigits == 0);
+        } else {
+            throw new IllegalArgumentException("올바른 입력값이 아닙니다.");
         }
 
         return answer;
