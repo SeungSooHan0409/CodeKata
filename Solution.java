e67622a128f293e88ea20b1e3291423e72617ade
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-22 14:51:38 +0900

    20

--- Solution.java
+++ Solution.java
@@ -2,29 +2,35 @@ package codekata;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Arrays;
+
+
 class Solution {
 
     public long solution(long n) {
         long answer = 0;
-        long distinct = 0;
-        int x = 0;
-        if(0 < n && n <= 50000000000000L) {
-            // 정수 제곱근이라면 해당 정수(i)로 나누면 몫으로 같은 정수(i)가 나와야함.
-            for(int i = 1; Math.pow(i,2) <= n; i++ ){
-                distinct = n/i;
-                if(distinct == i) {
-                    break;
-                }
-            }
-            // distinct 에 저장된 값이 정수 제곱근인지 판별하여 answer 값 설정
-            if(Math.pow(distinct,2) == n) {
-                answer = (long)Math.pow((distinct+1),2);
-            } else {
-                answer = -1;
-            }
-        } else {
-            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        int i = 0;
+        // n의 자릿수 세기
+        for(i = 1; (long)(n/Math.pow(10,i)) != 0;i++) {
         }
+
+        // 자릿수 만큼의 길이를 가진 배열 만들기
+        long[] arr = new long[i];
+
+        //배열에 값넣기
+        for (int j = 0; j < i ; j++) {
+            arr[j] = n%10;
+            n/=10;
+        }
+
+        // 배열을 오름차순으로 정렬하기 - Arrays 를 import 하지않으니 안돌아갔음.
+        Arrays.sort(arr);
+
+        // 정렬한 배열로 숫자만들기
+        for (i = 0; i < arr.length; i++) {
+            answer += (arr[i])*Math.pow(10,i);
+        }
+
         return answer;
     }
 
