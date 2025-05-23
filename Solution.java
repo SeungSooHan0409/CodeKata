8038d52f3de8403ece99dffc077c146ee43a0edf
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-23 21:34:06 +0900

    22

--- Solution.java
+++ Solution.java
@@ -7,20 +7,30 @@ import java.util.Arrays;
 
 class Solution {
 
-    public boolean solution(int x) {
-        boolean answer = true;
-        int SumOfDigits = 0;
-        int x2 = x;
-        if(0 < x && x <= 10000) {
-            // 자릿수의 합을 구하는 메서드 구현
-            for(int i = 1; (x2)%10 != 0 || (x2)/10 != 0; i++) {
-                SumOfDigits += (x2)%10;
-                (x2)/=10;
+    public long solution(int a, int b) {
+        long answer = 0;
+        // 숫자 범위 지정
+        if(-10000000 <= a && a <= 10000000 && -10000000 <= b && b <= 10000000) {
+            // b 가 a 보다 클 경우
+            if (b > a) {
+                for (int i = 0; b-i >= a; i++) {
+                    answer += b-i;
+                }
             }
-            // 나머지가 생기면 false 를 반환.
-            answer = (x%SumOfDigits == 0);
-        } else {
-            throw new IllegalArgumentException("올바른 입력값이 아닙니다.");
+            // a 가 b 보다 클 경우
+            else if(a > b) {
+                for (int i = 0; a-i >= b; i++) {
+                    answer += a-i;
+                }
+            }
+            // a = b 인 경우
+            else {
+                answer = a;
+            }
+        }
+        // 숫자 범위를 벗어난 경우
+        else {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
         }
 
         return answer;
