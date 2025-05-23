6ee14be4cc4c37973a945e59e129f3b5ea75bf29
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-23 21:34:25 +0900

    두정수의합

---
+++ SumOfIntegersBetween.java
@@ -0,0 +1,34 @@
+package codekata;
+
+public class SumOfIntegersBetween {
+
+    public long solution(int a, int b) {
+        long answer = 0;
+        // 숫자 범위 지정
+        if(-10000000 <= a && a <= 10000000 && -10000000 <= b && b <= 10000000) {
+            // b 가 a 보다 클 경우
+            if (b > a) {
+                for (int i = 0; b-i >= a; i++) {
+                    answer += b-i;
+                }
+            }
+            // a 가 b 보다 클 경우
+            else if(a > b) {
+                for (int i = 0; a-i >= b; i++) {
+                    answer += a-i;
+                }
+            }
+            // a = b 인 경우
+            else {
+                answer = a;
+            }
+        }
+        // 숫자 범위를 벗어난 경우
+        else {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
+
+        return answer;
+    }
+
+}
