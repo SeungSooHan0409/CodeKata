d1c59ed1620974546b55d2470c2ff80f5a04b8fe
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-05-28 09:20:37 +0900

    수박수박수

--- Lv2/Watermelon.java
+++ Lv2/Watermelon.java
@@ -1,4 +1,27 @@
 package codekata.Lv2;
 
 public class Watermelon {
+
+    public String solution(int n) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        // n 길이제한
+        if(0 < n && n <= 10000) {
+
+            // 문자열 생성
+            for(int i = 1; i <= n; i++) {
+                if(i%2 == 1) {
+                    sb.append("수");
+                } else {
+                    sb.append("박");
+                }
+            }
+        }
+
+        answer = sb.toString();
+
+        return answer;
+    }
+
 }
