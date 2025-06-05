1110d3425aac519596b0e4f224c518af65d7f683
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-06-05 15:30:36 +0900

    37

--- Solution.java
+++ Solution.java
@@ -5,22 +5,27 @@ import java.util.Arrays;
 
 class Solution {
 
-    public boolean solution(String s) {
-        boolean answer = true;
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][];
 
-        // 제한사항
-        if(1 <= s.length() && s.length() <= 8) {
+        // 제한 조건1 - 행의 길이제한
+        if(arr1.length <= 500) {
 
-            // 길이가 4 또는 6 일 경우만 계산
-            if(!(s.length() == 4) && !(s.length() == 6)) {
-                answer = false;
+            // 제한 조건2 - 열의 길이제한
+            for(int i=0; i < arr1.length; i++) {
+                if(arr1[i].length > 500) {
+                    throw new IllegalArgumentException("행렬의 열의 길이초과!");
+                }
+
             }
 
-            // 예외발생 여부로 answer 값 설정
-            try {
-                Integer examination = Integer.valueOf(s);
-            } catch(NumberFormatException e) {
-                answer = false;
+            // 중첩 for 문으로 덧셈 로직 작성
+            for(int i = 0; i < arr1.length; i++) {
+                answer[i] = new int[arr1[i].length];
+
+                for(int j = 0; j < arr1[i].length; j++) {
+                    answer[i][j] = arr1[i][j] + arr2[i][j];
+                }
             }
 
         }
