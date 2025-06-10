f52f3c3abc41956979c3a6fa373f23284ee3a7d8
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-06-10 09:56:40 +0900

    39

--- Solution.java
+++ Solution.java
@@ -6,30 +6,39 @@ import java.util.Scanner;
 
 class Solution {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
 
-        // n 제한 조건
-        if(1000 < a && a <= 0) {
-            throw new IllegalArgumentException("가로 길이 초과!");
+        // 제한 사항
+        if(1 <= n && n <= 1000000 && 1 <= m && m <= 1000000) {
+
+            // 최대 공약수 구하기
+            int divisor = 0;
+
+            for(int i = n; i >= 1; i--) {
+
+                // i 가 n 의 약수일 경우 최대공약수 인지 검사
+                if(n%i == 0 && m%i ==0) {
+                    divisor = i;
+                    break;
+                }
+
+            }
+
+
+            // 최소공배수 구하기
+            int multiple = n*m/divisor;
+
+
+            // 최소공배수, 최대공약수를 입력
+            answer[0] = divisor;
+            answer[1] = multiple;
+
+        } else {
+            throw new IllegalArgumentException("지정된 숫자 범위가 아닙니다.");
         }
 
-        // 가로 길이 만큼 * 추가
-        String n = "*".repeat(a);
-
-        int b = sc.nextInt();
-
-        // m 제한 조건
-        if(1000 < b && b <= 0) {
-            throw new IllegalArgumentException("세로 길이 초과!");
-        }
-
-        // 세로 길이만큼 반복
-        for(int i = 1; i <= b; i++) {
-            System.out.println(n);
-        }
-
+        return answer;
     }
 
 }
