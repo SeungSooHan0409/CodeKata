cae089407f0dd8dd73d7cb40f5944f5740868a24
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-06-17 09:23:21 +0900

    42

--- Solution.java
+++ Solution.java
@@ -6,44 +6,39 @@ import java.util.Scanner;
 
 class Solution {
 
-    public String solution(String s) {
-        String answer = "";
-        int count = 0;
+    public int solution(int[] number) {
+        int answer = 0;
+        int al = number.length;
 
-        // 문자열을 문자배열로 변환
-        char[] charArr = s.toCharArray();
+        // 제한사항
+        if(3 <= number.length && number.length <= 13) {
 
-        // 단어의 짝수번째 인덱스에는 대문자로 변경
-        for(int i = 0; i < s.length(); i++) {
+            // 전체 경우의 수 :  al*(al-1)*(al-2)/3!
+            // 중첩 for 문 으로 경우의 수 계산
+            for(int i = 0; i < al; i++) {
 
-            // 대문자 자리에 소문자 오면 로직전환 (if, else if)
-            if(i%2 == 0 && count%2 == 0) {
+                for(int j = 0; j < al; j++) {
 
-                if(charArr[i] == ' ') {
-                    count ++;
-                    continue;
-                } else {
-                    charArr[i] = Character.toUpperCase(charArr[i]);
+                    if(j == i) {
+                        continue;
+                    }
+
+                    for(int k = 0; k < al; k++) {
+                        if(k == i || k == j) {
+                            continue;
+                        }
+
+                        if(number[i] + number[j] + number[k] == 0) {
+                            answer++;
+                        }
+                    }
                 }
-
-            } else if(i%2 == 1 && count%2 == 1) {
-
-                if(charArr[i] == ' ') {
-                    count++;
-                    continue;
-                } else {
-                    charArr[i] = Character.toUpperCase(charArr[i]);
-                }
-
-                // 나머지 문자들은 소문자로 처리
-            } else {
-                charArr[i] = Character.toLowerCase(charArr[i]);
             }
 
         }
 
-        answer = String.valueOf(charArr);
-
+        // 3! 으로 answer 나누기
+        answer = answer / 6;
         return answer;
     }
 
