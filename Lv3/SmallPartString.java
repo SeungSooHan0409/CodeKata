2bb71971dba8a19d597665ddf262ba14afd35210
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-06-18 11:08:31 +0900

    크기가 작은 부분 문자열

---
+++ Lv3/SmallPartString.java
@@ -0,0 +1,58 @@
+package codekata.Lv3;
+
+import java.math.BigInteger;
+
+public class SmallPartString {
+
+    public int solution(String t, String p) {
+        int answer = 0;
+        BigInteger changeP = new BigInteger(p);
+
+        // 제한 사항
+        if(1 <= p.length() && p.length() < 18 && p.length() <= t.length() && t.length() <= 10000) {
+
+            // substring, compareTo, 반복문으로 해결
+            for(int i = 0; i + p.length() <= t.length(); i++) {
+
+                String partOfT = t.substring(i, i+(p.length()));
+
+                BigInteger changeT = new BigInteger(partOfT);
+
+                if(changeT.compareTo(changeP) <= 0) {
+                    answer += 1;
+                }
+
+            }
+
+        }
+
+        return answer;
+    }
+
+    // 또다른 답안
+//    public int solution(String t, String p) {
+//        int answer = 0;
+//        BigInteger changeP = new BigInteger(p);
+//
+//        // 제한 사항
+//        if(1 <= p.length() && p.length() < 18 && p.length() <= t.length() && t.length() <= 10000) {
+//
+//            // substring, compareTo, 반복문으로 해결
+//            for(int i = 0; i + p.length() <= t.length(); i++) {
+//
+//                String partOfT = t.substring(i, i+(p.length()));
+//
+//                BigInteger changeT = new BigInteger(partOfT);
+//
+//                if(changeT.compareTo(changeP) <= 0) {
+//                    answer += 1;
+//                }
+//
+//            }
+//
+//        }
+//
+//        return answer;
+//    }
+
+}
